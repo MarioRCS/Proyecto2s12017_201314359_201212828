8a61ac416e6f25bf4cdc5e664a2a3a5b2890b895
MarioRCS <mario69series24@gmail.com>
2017-04-25 22:02:24 -0600

    ArbolB en cada Nodo ArbolB
    integracion de un arbol b en cada nodo de la pagina del arbol b

--- ArbolB/ArbolB.py
+++ ArbolB/ArbolB.py
@@ -9,10 +9,13 @@ class NodoArbolB():
         self.ramas.append(None)
         self.ramas.append(None)
         self.ramas.append(None)
-        self.Numdatos=0
-        self.arbolB=None
-        self.AVL=None
         self.padre=None
+class Carpeta():
+
+    def __init__(self):
+        self.nombre=""
+        self.arbolb=None
+        self.avl=None
 
 
 class NodoLista():
@@ -66,37 +69,46 @@ class ArbolB():
 
     def ReacomodarRamas(self,pagina,padre,pagina2,dato):
         if self.estalleno(padre):
-            padre.claves.append(dato)
-            padre.claves.sort()
+            carpeta=Carpeta()
+            carpeta.nombre=dato
+            padre.claves.append(carpeta)
+            #padre.claves.sort()
+            self.Ordenar(padre.claves)
             padre.ramas.append(None)
             index=len(padre.claves)
-            while index > 1 and padre.claves[index-1] > dato:
+            while index > 1 and padre.claves[index-1].nombre > dato:
                 padre.ramas[index]=padre.ramas[index-1]
                 index-=1
             padre.ramas[index]=pagina2
-            temp=padre.claves[2]
+            temp=padre.claves[2].nombre
             self.Subir(padre,temp)
             padre.ramas.pop()
             padre.ramas.pop()
             padre.ramas.pop()
         else:
             if dato < padre.claves[0]:
-                padre.claves.append(dato)
-                padre.claves.sort()
+                carpeta=Carpeta()
+                carpeta.nombre=dato
+                padre.claves.append(carpeta)
+                #padre.claves.sort()
+                self.Ordenar(padre.claves)
                 index=len(padre.claves)
                 while index > 1:
                     padre.ramas[index]=padre.ramas[index-1]
                     index-=1
                 padre.ramas[1]=pagina2
             else:
-                padre.claves.append(dato)
-                padre.claves.sort()
+                carpeta=Carpeta()
+                carpeta.nombre=dato
+                padre.claves.append(carpeta)
+                #padre.claves.sort()
+                self.Ordenar(padre.claves)
                 if pagina!=self.raiz:
                     padre.ramas.append(None)
                 #aqui me quede
                 index=len(padre.claves)
                 while index > 0:
-                    if padre.claves[index-1]==dato:
+                    if padre.claves[index-1].nombre==dato:
                         padre.ramas[index]=pagina2
                     index-=1
 
@@ -104,22 +116,33 @@ class ArbolB():
 
     def Subir(self,pagina,dato):
         if len(pagina.claves)==4:
-            pagina.claves.append(dato)
-            pagina.claves.sort()
-            temp=pagina.claves[2]
+            carpeta=Carpeta()
+            carpeta.nombre=dato
+            pagina.claves.append(carpeta)
+            #pagina.claves.sort()
+            self.Ordenar(pagina.claves)
+            temp=pagina.claves[2].nombre
             pagina2=NodoArbolB()
-            pagina2.claves.append(pagina.claves[3])
-            pagina2.claves.append(pagina.claves[4])
+            carpeta1=Carpeta()
+            carpeta1.nombre=pagina.claves[3].nombre
+            carpeta2=Carpeta()
+            carpeta.nombre=pagina.claves[4].nombre
+            pagina2.claves.append(carpeta1)
+            pagina2.claves.append(carpeta2)
             pagina.claves.pop()
             pagina.claves.pop()
             pagina.claves.pop()
             self.ReacomodarRamas(pagina,pagina.padre,pagina2,temp)
         elif len(pagina.claves)==5:
-            temp=pagina.claves[2]
+            temp=pagina.claves[2].nombre
             #print(temp)
             pagina2=NodoArbolB()
-            pagina2.claves.append(pagina.claves[3])
-            pagina2.claves.append(pagina.claves[4])
+            carpeta=Carpeta()
+            carpeta.nombre=pagina.claves[3].nombre
+            carpeta1=Carpeta()
+            carpeta1.nombre=pagina.claves[4].nombre
+            pagina2.claves.append(carpeta)
+            pagina2.claves.append(carpeta1)
             pagina.claves.pop()
             pagina.claves.pop()
             pagina.claves.pop()
@@ -134,47 +157,66 @@ class ArbolB():
             if pagina==self.raiz:
 #                self.InsertarB(None,self.raiz,temp)
                 nuevo=NodoArbolB()
-                nuevo.claves.append(temp)
+                carpeta=Carpeta()
+                carpeta.nombre=temp
+                nuevo.claves.append(carpeta)
                 nuevo.ramas[0]=pagina
                 nuevo.ramas[1]=pagina2
                 self.raiz=nuevo
             else:
                 self.ReacomodarRamas(pagina,pagina.padre,pagina2,temp)
         else:
-            pagina.claves.append(dato)
-            pagina.claves.sort()
+            carpeta=Carpeta()
+            carpeta.nombre=dato
+            pagina.claves.append(carpeta)
+            #pagina.claves.sort()
+            self.Ordenar(pagina.claves)
 
     def InsertarB(self,padre,pagina,dato):
         if pagina==self.raiz:
             if pagina==None:
                 self.raiz=NodoArbolB()
-                self.raiz.claves.append(dato)
+                carpeta=Carpeta()
+                carpeta.nombre=dato
+                self.raiz.claves.append(carpeta)
             else:
-                if dato < self.raiz.claves[0]:
+                if dato < self.raiz.claves[0].nombre:
                     if self.raiz.ramas[0]!=None:
                         self.InsertarB(self.raiz,self.raiz.ramas[0],dato)
                     else:
                         if self.estalleno(self.raiz):
                             #dividir Nodo y asignar nueva raiz
-                            self.raiz.claves.append(dato)
-                            self.raiz.claves.sort()
+                            carpeta=Carpeta()
+                            carpeta.nombre=dato
+                            self.raiz.claves.append(carpeta)
+                            #self.raiz.claves.sort()
+                            self.Ordenar(self.raiz.claves)
                             nuevo=NodoArbolB()
-                            nuevo.claves.append(self.raiz.claves[2])
+                            carpeta1=Carpeta()
+                            carpeta1.nombre=self.raiz.claves[2].nombre
+                            nuevo.claves.append(carpeta1)
                             nuevo.ramas[0]=self.raiz
                             otrapagina=NodoArbolB()
-                            otrapagina.claves.append(self.raiz.claves[3])
-                            otrapagina.claves.append(self.raiz.claves[4])
+                            carpeta2=Carpeta()
+                            carpeta2.nombre=self.raiz.claves[3].nombre
+                            carpeta3=Carpeta()
+                            carpeta3.nombre=self.raiz.claves[4].nombre
+                            otrapagina.claves.append(carpeta2)
+                            otrapagina.claves.append(carpeta3)
                             self.raiz.claves.pop()
                             self.raiz.claves.pop()
                             self.raiz.claves.pop()
                             nuevo.ramas[1]=otrapagina
                             self.raiz=nuevo
                         else:
-                            self.raiz.claves.append(dato)
-                            self.raiz.claves.sort()
+                            carpeta=Carpeta()
+                            carpeta.nombre=dato
+                            self.raiz.claves.append(carpeta)
+                            #self.raiz.claves.sort()
+                            self.Ordenar(self.raiz.claves)
                 else:
                     index=len(self.raiz.claves)
-                    while dato < self.raiz.claves[index-1] and index > 0:
+                    while dato < self.raiz.claves[index-1].nombre and index > 0:
                         index-=1
                     if index > 0:
                         if self.raiz.ramas[index]!=None:
@@ -182,38 +224,60 @@ class ArbolB():
                         else:
                             if self.estalleno(self.raiz):
                                 #dividir Nodo y asignar nueva raiz
-                                self.raiz.claves.append(dato)
-                                self.raiz.claves.sort()
+                                carpeta=Carpeta()
+                                carpeta.nombre=dato
+                                self.raiz.claves.append(carpeta)
+                                #self.raiz.claves.sort()
+                                self.Ordenar(self.raiz.claves)
                                 nuevo=NodoArbolB()
-                                nuevo.claves.append(self.raiz.claves[2])
+                                carpeta1=Carpeta()
+                                carpeta1.nombre=self.raiz.claves[2].nombre
+                                nuevo.claves.append(carpeta1)
                                 nuevo.ramas[0]=self.raiz
                                 otrapagina=NodoArbolB()
-                                otrapagina.claves.append(self.raiz.claves[3])
-                                otrapagina.claves.append(self.raiz.claves[4])
+                                carpeta2=Carpeta()
+                                carpeta2.nombre=self.raiz.claves[3].nombre
+                                carpeta3=Carpeta()
+                                carpeta3.nombre=self.raiz.claves[4].nombre
+                                otrapagina.claves.append(carpeta2)
+                                otrapagina.claves.append(carpeta3)
                                 self.raiz.claves.pop()
                                 self.raiz.claves.pop()
                                 self.raiz.claves.pop()
                                 nuevo.ramas[1]=otrapagina
                                 self.raiz=nuevo
                             else:
-                                self.raiz.claves.append(dato)
-                                self.raiz.claves.sort()
+                                carpeta=Carpeta()
+                                carpeta.nombre=dato
+                                self.raiz.claves.append(carpeta)
+                                #self.raiz.claves.sort()
+                                self.Ordenar(self.raiz.claves)
         else:
             pagina.padre=padre
-            if dato < pagina.claves[0]:
+            if dato < pagina.claves[0].nombre:
                 if pagina.ramas[0]!=None:
                     self.InsertarB(pagina,pagina.ramas[0],dato)
                 else:
                     self.Subir(pagina,dato)
             else:
                 index=len(pagina.claves)
-                while dato < pagina.claves[index-1] and index > 0:
+                while dato < pagina.claves[index-1].nombre and index > 0:
                     index-=1
                 if index > 0:
                     if pagina.ramas[index]!=None:
                         self.InsertarB(pagina,pagina.ramas[index],dato)
                     else:
                         self.Subir(pagina,dato)
+
+    def Ordenar(self,vector):
+        temp=[]
+        for elemento in vector:
+            temp.append(elemento.nombre)
+        temp.sort()
+        index2=0
+        for elemento2 in temp:
+            vector[index2].nombre=temp[index2]
+            index2+=1
 
 
 
@@ -228,10 +292,42 @@ class ArbolB():
 
 
 
+    def buscar(self,raiz,dato):
+        encontrado=False
+        if dato<raiz.claves[0].nombre:
+            if raiz.ramas[0]!=None:
+                return self.buscar(raiz.ramas[0],dato)
+            else:
+                print("el dato no se encuentra en el arbol")
+        else:
+            index=len(raiz.claves)
+            while index>0 and dato<=raiz.claves[index-1].nombre:
+                if dato==raiz.claves[index-1].nombre:
+                    #print("el dato: "+str(dato)+" fue encontrado")
+                    return raiz
+                    encontrado=True
+                index-=1
+            if encontrado==False:
+                if index>0:
+                    if raiz.ramas[index]!=None:
+                        return self.buscar(raiz.ramas[index],dato)
+                    else:
+                        print("el dato no se encuentra en el arbol")
+            else:
+                pass
+
+    def CrearenRuta(self,ruta,dato):
+        self.buscar(self.primero.carpeta,ruta).claves[self.IndicedelNodo(self.buscar(self.primero.carpeta,ruta).claves,ruta)].arbolb=self.raiz
+        self.InsertarB(None,self.buscar(self.primero.carpeta,ruta).claves[self.IndicedelNodo(self.buscar(self.primero.carpeta,ruta).claves,ruta)].arbolb,dato)
 
 
 
-
+    def IndicedelNodo(self,vector,dato):
+        temp=[]
+        for elemento in vector:
+            temp.append(elemento.nombre)
+        indice=temp.index(dato)
+        return indice
 
 Ar=ArbolB()
 Ar.InsertarUsuario("mario",123)
@@ -249,5 +345,7 @@ Ar.CarpetaenUsuario("luna",1)
 Ar.raiz=None
 Ar.CarpetaenUsuario("Roberto",100)
 
-print(Ar.primero.carpeta.ramas[0].claves)
+Ar.CrearenRuta(4,10)
+
+print(Ar.primero.carpeta.ramas[0].claves[0].arbolb.claves[0].nombre)
 
